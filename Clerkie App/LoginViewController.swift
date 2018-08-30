572d51305dde49713cdb2c102a5181c8b943647c
ramsricharan <ramcharan.syr@gmail.com>
2018-08-30 14:27:15 -0700

    Deleted Storyboard and add programmatic launching point

--- Clerkie App/LoginViewController.swift
+++ Clerkie App/LoginViewController.swift
@@ -13,7 +13,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.yellow
+        view.backgroundColor = UIColor.orange
         // Do any additional setup after loading the view, typically from a nib.
     }
 
