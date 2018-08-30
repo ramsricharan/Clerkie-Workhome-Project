292e2a2b51f602d66319a4a3e8b4effd367fd3db
ramsricharan <ramcharan.syr@gmail.com>
2018-08-30 13:39:15 -0700

    Initial Commit

---
+++ Clerkie App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clerkie App
+//
+//  Created by Ram Sri Charan on 8/30/18.
+//  Copyright © 2018 Ram Sri Charan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
