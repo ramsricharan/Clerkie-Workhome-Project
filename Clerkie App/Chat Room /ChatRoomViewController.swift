db9735ac51a75a1c1319d7c96bb9d9028ca8cccd
ramsricharan <ramcharan.syr@gmail.com>
2018-08-31 19:57:52 -0700

    Updated File Structure

--- Clerkie App/Chat Room /ChatRoomViewController.swift
+++ Clerkie App/Chat Room /ChatRoomViewController.swift
@@ -5,17 +5,17 @@
 //  Created by Ram Sri Charan on 8/31/18.
 //  Copyright © 2018 Ram Sri Charan. All rights reserved.
 //
-
 import UIKit
 
 class ChatRoomViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.yellow
         // Do any additional setup after loading the view.
     }
-
-
-
+    
+    
+    
 }
+
